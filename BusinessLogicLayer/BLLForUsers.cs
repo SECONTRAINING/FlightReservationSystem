a3eb49023f84a9400eafb12ef00f4f70da364e64
sugumar1502 <147132519+sugumar1502@users.noreply.github.com>
2023-10-12 18:55:48 +0530

    Add files via upload

---
+++ BusinessLogicLayer/BLLForUsers.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer;
+using DataModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public  class BLLForUsers: IBLLForUsers
+    {
+        IDALForUsers _dalForUsers;
+        public BLLForUsers(IDALForUsers dALForUsers) 
+        {
+            _dalForUsers= dALForUsers;
+        }
+
+        public List<FlightDetails> getFlightsByDayAndLocation(string day, string source, string destination)
+        {
+            try
+            {
+                return _dalForUsers.getFlightsByDayAndLocation(day, source, destination);
+            }
+            catch(Exception ex)
+            {
+                throw(ex);
+            }
+        }
+    }
+}
---
+++ BusinessLogicLayer/IBLLForObject.cs
@@ -0,0 +1,55 @@
+﻿using DataModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public interface IBLLForAdmin
+    {
+        List<User> GetAllUsers();
+
+        string InsertUser(User user);
+
+        string DeleteUser(int UserId);
+
+        int ValidateLogin(string UserName, string Password);
+    }
+
+    public interface IBLLForFlightDetails
+    {
+        List<FlightDetails> GetAllFlightDetails();
+
+        string insertFlightDetails(FlightDetails flightDetails);
+        string deleteFlightDetailsById(int FlightId);
+        FlightDetails GetFlightDetailsById(int FlightId);
+
+        FlightDetails getFlightByDestination(string source,string  destination);
+        List<string> getAllStations();
+        string UpdateFlightDetailsById(int id, FlightDetails flightDetails);
+
+        int [] getFLightIdbySourcedestination(string source, string destination);
+        List<FlightDetails> GetFlightDetailsByArrayId(int[] FlightId);
+    }
+
+    public interface IBLLForFlightSchedule
+    {
+        List<FlightSchedule> getAllFlightSchedule();
+        FlightSchedule getFlightScheduleByScheduleId(int scheduleId);
+        FlightSchedule getFlightScheduleByFligthId(int flighthId);
+        string addFlightSchedule(FlightSchedule flightSchedule);
+        string deleteFlightScheduleByScheduleId(int id);
+        string deleteFlightScheduleByFlightId(int id);
+        int [] getFlightIdByScheduleDay(string day);
+
+       // getFLightDetailsbySourcedestination
+
+    }
+
+    public interface IBLLForUsers
+    {
+        List<FlightDetails> getFlightsByDayAndLocation(string day, string source, string destination);
+    }
+}
