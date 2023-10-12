3003f68d9bbcf3e31349ebd39ae0b5bd8b21494e
sugumar1502 <147132519+sugumar1502@users.noreply.github.com>
2023-10-12 10:42:56 +0530

    Add files via upload

---
+++ DataModelLayer/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiFlightReservationLoginModel
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
