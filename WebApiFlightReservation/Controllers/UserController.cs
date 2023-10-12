7745289caf2e86ff4d97853247d512a9c4da7af3
sugumar1502 <147132519+sugumar1502@users.noreply.github.com>
2023-10-12 18:59:10 +0530

    Add files via upload

---
+++ WebApiFlightReservation/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLogicLayer;
+using DataModelLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiFlightReservation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        IBLLForUsers _bLLForUsers;
+        public UserController(IBLLForUsers bLLForUsers) 
+        {
+            _bLLForUsers = bLLForUsers;
+        }
+
+
+        [HttpGet]
+        [Route("getFlightsByDayAndLocation")]
+        public List<FlightDetails> getFlightsByDayAndLocation(string day, string source,string destination)
+        {
+            return _bLLForUsers.getFlightsByDayAndLocation(day, source, destination);
+        }
+
+       
+
+
+    }
+
+    
+}
