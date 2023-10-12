0d1e5a2f7e2b2a1b19b3e9c1edeb26f51a59f345
sugumar1502 <147132519+sugumar1502@users.noreply.github.com>
2023-10-12 10:35:48 +0530

    Delete DataModelLayer directory

--- DataModelLayer/LoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApiFlightReservation.Controllers
-{
-    public class LoginModel
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
